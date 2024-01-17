a39173b5f730608adfaba304b6badc0272cf5823
Segoy <segoy.thiel@protonmail.com>
2024-01-17 12:34:33 +0100

    Add Counter to local Storage with Effects

--- src/app/store/counter.selectors.ts
+++ src/app/store/counter.selectors.ts
@@ -1,6 +1,6 @@
 import {createSelector} from "@ngrx/store";
 
-export const selectCount = (state: { counter: number }) => state.counter
+export const selectCount = (state: { counter: number }) => state.counter;
 export const selectDoubleCount = createSelector(
   selectCount,
   (state) => state * 2
